a8e64413817b4278a80fa4d7f590225b9fe1ca05
Luis Lopez <luislmf@gmail.com>
2022-04-05 10:14:40 +0100

    Script de lectura remota de los EXCEL con los datos

---
+++ code/helper01_lee-excels.R
@@ -0,0 +1,26 @@
+# Lectura de los archivos donde se almacena la información descargada de TarSys
+# Nota: la información se almacena en libros de EXCEL
+
+
+# Librerias y Variables ---------------------------------------------------
+
+library(tidyverse)
+library(readxl)
+
+ruta_tarsys <- "\\\\bender\\departamento\\Tarsys"
+patron_tarsys <- "Mensuales"
+
+
+# Acceso a los archivos ---------------------------------------------------
+
+# lista de archivos a procesar
+archivos_tarsys <-
+  list.files(
+    path = ruta_tarsys,
+    pattern = patron_tarsys,
+    full.names = T
+  ) %>% 
+  file.info() %>% 
+  as_tibble(
+    rownames = "archivo"
+  )
