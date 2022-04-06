2f598a953be808e8585f88edfeb90c29fc367af2
Luis Lopez <luislmf@gmail.com>
2022-04-06 11:42:00 +0100

    Actualizado, las funciones se guardan en un script y se llaman a través de "source"

--- code/helper01_lee-excels.R
+++ code/helper01_lee-excels.R
@@ -2,84 +2,19 @@
 # Nota: la información se almacena en libros de EXCEL
 
 
-# Librerias y Variables ---------------------------------------------------
+# Librerías y Variables ---------------------------------------------------
 
 library(tidyverse)
 library(lubridate)
+library(here)
 library(readxl)
 
 ruta_tarsys <- "\\\\bender\\departamento\\Tarsys"
 patron_tarsys <- "Mensuales"
 
+# funciones propias de este proceso
+source(file = here("helper01_funs.R"))
 
-# Funciones ---------------------------------------------------------------
-
-# Filtro los archivos almacenados en el directorio remoto Tarsys y me quedo
-# con los que contienen información anual de las instalaciones.
-# Estos son archivos EXCEL con una hoja por mes con los datos de los contadores
-#
-# Inputs:
-#   - ruta hasta los archivos
-#   - patrón para descargar sólo los de los datos de los contadores
-#
-# Outputs:
-#   - lista de archivos a procesar, son los últimos de cada anualidad
-
-filtrar_archivos <- function(ruta, patron){
-  archivos <-
-    list.files(
-      path = ruta,
-      pattern = patron,
-      full.names = T
-    ) %>% 
-    as_tibble() %>%
-    # el nombre del archivo contiene información de la anualidad y mensualidad
-    separate(
-      col = value,
-      into = c("aaaa", "periodicidad", "mes", "version", "tipo_archivo"),
-      sep = "\\.",
-      remove = F
-    ) %>% 
-    mutate(
-      mes = str_extract(mes, pattern = "\\d+") %>% as.integer()
-    ) %>% 
-    group_by(aaaa, mes) %>% 
-    # filtro la última versión de cada mes
-    filter(
-      version == max(version)
-    ) %>% 
-    ungroup() %>% 
-    select(value)
-  
-  return(archivos)
-}
-
-extrae_datos <- function(archivos_lst) {
-  # contenedor de los datos
-  ds_all <- tibble()
-  
-  for (i in seq_along(archivos_lst[[1]])) {
-    ds <-
-      read_xls(
-        path = archivos_lst[[i,1]],
-        sheet = as.numeric(archivos_lst[[i,3]]),
-        range = cell_cols(c(2:8)),
-        col_names = c("planta", "dummy1", "dummy2", "reac_gen_kw", "reac_cons_kw", "consumo_kw", "generacion_kw"),
-        skip = 2
-      ) %>% 
-      mutate(
-        anualidad = archivos_lst[[i,2]],
-        mes = archivos_lst[[i,3]],
-        fecha = ym(str_c(anualidad, mes, sep = "-")) %>% 
-          rollforward()
-      )
-    ds_all <- bind_rows(ds_all, ds)
-    rm(ds)
-    
-  }
-  
-  return(ds_all)
-}
 
 # Acceso a los datos ------------------------------------------------------
 
@@ -88,15 +23,9 @@ archivos_tarsys <-
   filtrar_archivos(
     ruta = ruta_tarsys,
     patron = patron_tarsys
-  ) %>% 
-  # añado mes y año
-  mutate(
-    archivo = value,
-    anualidad = str_extract(archivo, pattern = "\\d+"),
-    mes = str_sub(archivo, start = 46, end = 47),
-    .keep = "none"
   )
 
+# datos
 ds_tarsys <- 
   extrae_datos(archivos_tarsys) %>% 
   select(fecha, planta, ends_with("kw")) %>% 
